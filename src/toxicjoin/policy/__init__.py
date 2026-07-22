b119fa7515ba8458ca64acf2201ee253882de81e
Zaid Hijazi <z3x1337@gmail.com>
2026-07-23 00:19:17 +0300

    Export canonical policy loader

--- src/toxicjoin/policy/__init__.py
+++ src/toxicjoin/policy/__init__.py
@@ -1,5 +1,10 @@
 """ToxicJoin policy package."""
 
-from toxicjoin.policy.engine import PolicyConfig, PolicyEngine, load_policy
+from toxicjoin.policy.engine import (
+    PolicyConfig,
+    PolicyEngine,
+    default_policy_path,
+    load_policy,
+)
 
-__all__ = ["PolicyConfig", "PolicyEngine", "load_policy"]
+__all__ = ["PolicyConfig", "PolicyEngine", "default_policy_path", "load_policy"]
