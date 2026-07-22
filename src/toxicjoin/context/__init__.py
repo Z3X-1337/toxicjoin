6dffdf65de6042b78200a7d957187878f8afc869
Zaid Hijazi <z3x1337@gmail.com>
2026-07-22 23:54:15 +0300

    Add context resolver package

---
+++ src/toxicjoin/context/__init__.py
@@ -0,0 +1,13 @@
+"""Governed metadata resolution for ToxicJoin."""
+
+from toxicjoin.context.fixture import (
+    ContextResolution,
+    FixtureContextResolver,
+    load_fixture_catalog,
+)
+
+__all__ = [
+    "ContextResolution",
+    "FixtureContextResolver",
+    "load_fixture_catalog",
+]
